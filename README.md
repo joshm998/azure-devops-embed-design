8a55810a8f73ad79fbffc591736bcd757e9c2ef2
Josh Mangiola <joshm982@gmail.com>
2022-12-10 00:54:34 +1030

    initial Commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Azure DevOps Embed Design
+
+Azure DevOps Embed Design is a extension that allows users to seamlessly integrate their Figma designs into Azure DevOps. With this extension, designers can easily share their designs with the development team, who can then use the designs to build and implement the user interface for a project. This extension makes it possible for designers to embed their designs into Azure DevOps work items, allowing developers and designs to keep on the same page.
+
+
+## Running Locally for Development
+
+### Prerequisites
+
+Download and install the following tools
+
+1. [Visual Studio Code](https://code.visualstudio.com/download)
+1. [Firefox](https://www.mozilla.org/firefox/) (because the VS Code Debugger for Chrome extension [doesn't support iframes](https://github.com/microsoft/vscode-chrome-debug/issues/786) yet)
+1. The [Debugger for Firefox](https://marketplace.visualstudio.com/items?itemName=hbenl.vscode-firefox-debug) VS Code extension
+1. The [tfx-cli](https://www.npmjs.com/package/tfx-cli) npm package
+1. The [webpack](https://www.npmjs.com/package/webpack) npm package
+1. The [webpack-dev-server](https://www.npmjs.com/package/webpack-dev-server) npm package
+
+> If you would prefer not to install the npm packages globally, you can add them to devDependencies in your `package.json` file and invoke them with scripts. You can use the [package.json](./package.json) in this repo as a template for scripts and to ensure you have the correct versions of packages in your extension.
+
+Before we deploy the extension, we need to install its dependencies and compile the code using the following commands:
+
+```shell
+npm install
+webpack --mode development
+```
+
+You will need to deploy your extension to the marketplace at least once using the following command:
+
+```shell
+tfx extension publish --manifest-globs vss-extension.json --overrides-file configs/dev.json --token [token]
+```
+
+> The `[token]` here is an Azure DevOps PAT (personal access token) with the **Marketplace (Publish)** scope and access set to **All accessible organizations**. For more information, see [Authenticate access with personal access tokens](https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate).
+
+After the extension is published, share it with your Azure DevOps organization and install it. Navigate to a project and you will find a new hub called "Hello World!" When you click it, you will notice that the hub won't load correctly. It isn't loading because the extension is configured to load all its resources (html, images, etc.) from `localhost:3000`, but the dev server isn't running yet.
+
+Start the webpack-dev-server with:
+
+```shell
+webpack-dev-server --mode development
+```
+
+Now if you go to `localhost:3000` in your browser, you should get an untrusted certificate error page. Select **Advanced** and then select **Accept the Risk and Continue**.
+
+Go back to Azure DevOps and reload. Your extension should now load correctly and any changes to the source code will cause webpack to recompile and reload the extension automatically.
+
+Although most code changes will be reflected immediately, you may still need to occasionally update your extension in the marketplace. The dev extension loads all its resources from the webpack-dev-server, but the manifest itself is being loaded from the published code. Therefore, any changes to the manifest file will not be properly reflected in Azure DevOps until the extension has been republished.
---
+++ src/design/design.tsx
@@ -0,0 +1,129 @@
+import "azure-devops-ui/Core/override.css";
+import "./design.scss";
+
+import { WorkItemOptions, WorkItemTrackingServiceIds, IWorkItemFormService } from "azure-devops-extension-api/WorkItemTracking/WorkItemTrackingServices";
+import * as SDK from "azure-devops-extension-sdk";
+import { Page } from "azure-devops-ui/Page";
+import { SingleLayerMasterPanel, SingleLayerMasterPanelHeader } from "azure-devops-ui/MasterDetails";
+import { Tooltip } from "azure-devops-ui/TooltipEx";
+import { IListItemDetails, List, ListItem, ListSelection } from "azure-devops-ui/List";
+
+import { ZeroData } from "azure-devops-ui/ZeroData";
+import { useState, useEffect } from "react";
+
+import * as ReactDOM from "react-dom";
+import React = require("react");
+import { ArrayItemProvider } from "azure-devops-ui/Utilities/Provider";
+
+
+const URL_REGEX = (/https:\/\/([\w\.-]+\.)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/)
+
+const Hub: React.FC<{}> = (props: any) => {
+    const [designs, setDesigns] = useState([] as string[]);
+    const [selectedItem, setSelectedItem] = useState(null as String | null);
+    const [selection] = React.useState(new ListSelection({ selectOnFocus: false }));
+
+    const [itemProvider, setItemProvider] = React.useState(new ArrayItemProvider(designs));
+
+    useEffect(() => {
+        SDK.init().then(async () => {
+            await registerEvents();
+        });
+    }, []);
+
+    const registerEvents = async () => {
+        var workItemFormService = await SDK.getService<IWorkItemFormService>(WorkItemTrackingServiceIds.WorkItemFormService);
+        var description = (await workItemFormService.getFieldValue("System.Description", ({} as WorkItemOptions)) as string);
+        var doc = document.createElement("html");
+        doc.innerHTML = description;
+        var links = doc.getElementsByTagName("a");
+        var urls = [] as string[];
+
+        for (var i = 0; i < links.length; i++) {
+            var url = links[i].getAttribute("href");
+
+            const match = url!.match(URL_REGEX)
+            if (match) {
+                urls.push(url!);
+            }
+        }
+
+        setDesigns(urls);
+        setItemProvider(new ArrayItemProvider(urls));
+
+        console.log(selection);
+
+        console.log(urls);
+        console.log(selectedItem)
+    };
+
+    const renderHeader = () => {
+        return <SingleLayerMasterPanelHeader title="Designs" />;
+    };
+
+    const renderContent = (selection: ListSelection, itemProvider: ArrayItemProvider<string>) => {
+        return (
+            <List
+                ariaLabel={"List of Designs"}
+                itemProvider={itemProvider}
+                selection={selection}
+                renderRow={renderListItem}
+                width="100%"
+                onFocus={(a, b) => setSelectedItem(b.data)}
+                singleClickActivation={true}
+            />
+        );
+    };
+
+    const renderListItem = (
+        index: number,
+        item: string,
+        details: IListItemDetails<string>,
+        key?: string
+    ): JSX.Element => {
+
+        return (
+            <ListItem
+                className="master-example-row"
+                key={key || "list-item" + index}
+                index={index}
+                details={details}
+            >
+                <div className="master-example-row-content flex-row flex-center h-scroll-hidden">
+                    <Tooltip overflowOnly={true}>
+                        <div className="primary-text text-ellipsis">{item}</div>
+                    </Tooltip>
+                </div>
+            </ListItem>
+        );
+    };
+
+    return (
+        <Page className="flex-grow">
+            {designs.length > 0 ?
+                <div className="master-example-scroll-container flex-row">
+                    <SingleLayerMasterPanel
+                        className="master-example-panel show-on-small-screens"
+                        renderHeader={renderHeader}
+                        renderContent={() => renderContent(selection, itemProvider)}
+                    />
+                    {selectedItem != null ? <Page className="flex-grow single-layer-details">
+                        <iframe className="design-frame" src={`https://www.figma.com/embed?embed_host=azuredevops&url=${selectedItem}`} />
+                    </Page> : <div></div>}
+
+                </div> :
+                <ZeroData iconProps={{
+                    iconName: "Sad"
+                }} imageAltText="Sad Face" primaryText="No Designs Found..." secondaryText={<span>
+                    Check out the{" "}
+                    <a rel="nofollow noopener" target="_blank" href="https://github.com/joshm998/azure-devops-embed-design">
+                        documentation
+                    </a>{" "}
+                    to see how to add designs.
+                </span>} />}
+        </Page>
+    );
+
+};
+
+ReactDOM.render(<Hub />, document.getElementById("root"));
